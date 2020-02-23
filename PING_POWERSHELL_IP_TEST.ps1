19bb72d569603a06f7e0db6796272a1702bb15aa
brewst001 <loganbrewster91@gmail.com>
2020-02-22 22:27:26 -0700

    Update and rename PING_POWERSHELL_DNS_TEST.ps1 to PING_POWERSHELL_IP_TEST.ps1

--- PING_POWERSHELL_IP_TEST.ps1
+++ PING_POWERSHELL_IP_TEST.ps1
@@ -1,3 +1,4 @@
+# This is used to verify if all IP's in a specific file are reachable. paste each separate IP line by line.
 Start-Transcript -Path .\log.txt
 (Get-Content .\IPAddresses.txt) | ForEach {Write-Host $_, "-", ([System.Net.NetworkInformation.Ping]::new().Send($_)).Status}
 Stop-Transcript
