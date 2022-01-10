795ec7f1332fefe72c3c484af41d29e5ab740429
brewst001 <loganbrewster91@gmail.com>
2022-01-10 14:31:08 -0700

    Update PING_POWERSHELL_IP_TEST.ps1

--- PING_POWERSHELL_IP_TEST.ps1
+++ PING_POWERSHELL_IP_TEST.ps1
@@ -1,6 +1,45 @@
-# This is used to verify if all IP's in a specific file are reachable. paste each separate IP line by line.
+# This is used to verify if all IP's in a specific file are reachable. paste each separate IP line by line. 
 Start-Transcript -Path .\log.txt
 (Get-Content .\IPAddresses.txt) | ForEach {Write-Host $_, "-", ([System.Net.NetworkInformation.Ping]::new().Send($_)).Status}
 Stop-Transcript
 
 Pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PS. I hate powershell.
